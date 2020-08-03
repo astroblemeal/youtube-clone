a2b1258cf1caa6640e6b29cb5ebd5aa7518a8891
Alexander <alexander@Alex.local>
2020-08-03 20:37:03 +0700

    basic card layout

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,14 +1,59 @@
 import React from 'react'
-import "./Sidebar.css";
+import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
-function RecommendedVieos() {
+
+function RecommendedVideos() {
   return (
-    <div className="recommendedVieos">
-      <h1>This is where videos go</h1>
+    <div className="recommendedVideos">
 
 
+        <h2> Recommended</h2>
+
+        <div className="recommendedVideos_videos">
+          <VideoCard
+          title="John Mayer Tomo Fujita Berklee"
+          views="2.4m"
+          timestamp="4 months ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxU_3TzgawKDPE-6wH3SWYCKHxUcCZ7_h449rBU=s100-c-k-c0xffffffff-no-rj-mo"
+          image="https://lh4.googleusercontent.com/proxy/sT8Gz5I59rM2xuwvgnOAHa9tOjuq6CY7WnjLlbkGeHp-jc67nGbuCVj_JQQp2DHsZe4JimDcWDRaqYONzWnKiXNfNOQUbgcWL7npDQ"
+          />
+
+          <VideoCard
+          title="John Mayer Tomo Fujita Berklee"
+          views="2.4m"
+          timestamp="4 months ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxU_3TzgawKDPE-6wH3SWYCKHxUcCZ7_h449rBU=s100-c-k-c0xffffffff-no-rj-mo"
+          image="https://lh4.googleusercontent.com/proxy/sT8Gz5I59rM2xuwvgnOAHa9tOjuq6CY7WnjLlbkGeHp-jc67nGbuCVj_JQQp2DHsZe4JimDcWDRaqYONzWnKiXNfNOQUbgcWL7npDQ"
+          />
+
+          <VideoCard
+          title="John Mayer Tomo Fujita Berklee"
+          views="2.4m"
+          timestamp="4 months ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxU_3TzgawKDPE-6wH3SWYCKHxUcCZ7_h449rBU=s100-c-k-c0xffffffff-no-rj-mo"
+          image="https://lh4.googleusercontent.com/proxy/sT8Gz5I59rM2xuwvgnOAHa9tOjuq6CY7WnjLlbkGeHp-jc67nGbuCVj_JQQp2DHsZe4JimDcWDRaqYONzWnKiXNfNOQUbgcWL7npDQ"
+          />
+
+          <VideoCard
+          title="John Mayer Tomo Fujita Berklee"
+          views="2.4m"
+          timestamp="4 months ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxU_3TzgawKDPE-6wH3SWYCKHxUcCZ7_h449rBU=s100-c-k-c0xffffffff-no-rj-mo"
+          image="https://lh4.googleusercontent.com/proxy/sT8Gz5I59rM2xuwvgnOAHa9tOjuq6CY7WnjLlbkGeHp-jc67nGbuCVj_JQQp2DHsZe4JimDcWDRaqYONzWnKiXNfNOQUbgcWL7npDQ"
+          />
+
+          <VideoCard
+          title="John Mayer Tomo Fujita Berklee"
+          views="2.4m"
+          timestamp="4 months ago"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxU_3TzgawKDPE-6wH3SWYCKHxUcCZ7_h449rBU=s100-c-k-c0xffffffff-no-rj-mo"
+          image="https://lh4.googleusercontent.com/proxy/sT8Gz5I59rM2xuwvgnOAHa9tOjuq6CY7WnjLlbkGeHp-jc67nGbuCVj_JQQp2DHsZe4JimDcWDRaqYONzWnKiXNfNOQUbgcWL7npDQ"
+          />
+
+      </div>
     </div>
   )
 }
 
-export default RecommendedVieos
+export default RecommendedVideos
