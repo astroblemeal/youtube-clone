8d10c731d46919ef20cbec605e192c2e973e9105
Alexander <alexander@Alex.local>
2020-08-19 23:12:49 +0700

    work in progress

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './SearchPage.css';
-
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 function SearchPage() {
   return (
-    <div className="SearchPage">
+    <div className="searchPage">
+      <div className="searchPage_filter">
+        <TuneOutlinedIcon />
 
+        <h2>FILTER</h2>
+
+      </div>
+      <hr />
     </div>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
